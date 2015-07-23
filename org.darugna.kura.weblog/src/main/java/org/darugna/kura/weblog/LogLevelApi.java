a2926b4e087dc58929c97b7b4b6c38ddf5005ef7
Alessandro Da Rugna <alessandro.darugna@gmail.com>
2015-07-23 19:49:14 +0200

    Draft of Jquery table population

--- org.darugna.kura.weblog/src/main/java/org/darugna/kura/weblog/LogLevelApi.java
+++ org.darugna.kura.weblog/src/main/java/org/darugna/kura/weblog/LogLevelApi.java
@@ -1,5 +1,9 @@
 package org.darugna.kura.weblog;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Level;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,17 +24,46 @@ public final class LogLevelApi extends HttpServlet {
 	
 	public final void doGet(HttpServletRequest request, HttpServletResponse response) {
 		response.setContentType("application/json");
+		PrintWriter out = null;
+		
+		try {
+			out = response.getWriter();
+			JSONArray json = new JSONArray();
+			Map<String,String>[] orderedLoggers = getLoggersLevel();
+			for (int i = 0; i < orderedLoggers.length; ++i) {
+				json.put(orderedLoggers[i]);
+			}
+					
+			out.print(json.toString());
+			out.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
 	}
 	
 	
-	private Map<String,String> getLoggersLevel() {
+	private Map<String,String>[] getLoggersLevel() {
 		Enumeration<?> currentLoggers = org.apache.log4j.LogManager.getCurrentLoggers();
 		HashMap<String,String> loggersLevel = new HashMap<>();
+		ArrayList<String> loggerNames = new ArrayList<String>();
 		while (currentLoggers.hasMoreElements()) {
 			org.apache.log4j.Logger log4jLogger = (org.apache.log4j.Logger)currentLoggers.nextElement();
 			loggersLevel.put(log4jLogger.getName(), log4jLogger.getEffectiveLevel().toString());
+			loggerNames.add(log4jLogger.getName());
 		}
-		return loggersLevel;
+		Collections.sort(loggerNames);
+		Map<String,String>[] orderedLoggers = new Map[loggerNames.size()];
+		for (int i = 0; i < loggerNames.size(); ++i) {
+			orderedLoggers[i] = new HashMap<String,String>();
+			orderedLoggers[i].put("name", loggerNames.get(i));
+			orderedLoggers[i].put("level", loggersLevel.get(loggerNames.get(i)));
+		}
+		return orderedLoggers;
 	}
 
 }
--- org.darugna.kura.weblog/src/main/webapp/weblogjs/index.js
+++ org.darugna.kura.weblog/src/main/webapp/weblogjs/index.js
@@ -1,11 +1,44 @@
 $(document).ready(function () {
+	
+	function createComboBox(loggerName, loggerLevel) {
+		var comboHtml = '<select id="combo.' + loggerName + '">';
+
+		levels = ['TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL'];
+		for(var l in levels) {
+			comboHtml += '<option value="' + levels[l] + '">' + levels[l] + "</option>";
+		}
+		
+		return comboHtml;
+	}
+	
+	
+	
 	$.ajax({
 		url: 'weblogapi',
 		type: 'GET',
 		dataType: 'json',
 	})
 	.done(function(data) {
-		alert('DATA');
+		console.log(data);
+		for (var i in data) {
+			console.log(i);
+			
+			var txt = '<tr><td>';
+			txt += data[i].name;
+			txt += '</td><td>';
+			txt += createComboBox(data[i].name ,data[i].level);
+			txt += '</td></tr>';
+//    		for (var key in data[i]){
+//    			txt+= '<tr>';
+//    			txt+= '<td>'+key+'</td>';
+//    			txt+= '<td>'+data[i][key]+'</td>';
+//    			txt+= '</tr>';
+//    		}
+//    		txt+= '</tr>';
+			$.parseHTML(txt)
+    		$('#lltable').append(txt);
+		}
+		
 	})
 	.fail(function(jqxhr, status, err) {
         alert(jqxhr);
